270f910273fa3d8189a9b3f544faffa53a76d084
Julia Brown <juliabrown@MacBook-Pro.local>
2024-02-02 14:52:45 +1100

    Reversed list

--- Reverse_linked_list.py
+++ Reverse_linked_list.py
@@ -11,6 +11,7 @@ class SinglyLinkedListNode:
         self.data = node_data
         self.next = None
 
+
 class SinglyLinkedList:
     def __init__(self):
         self.head = None
@@ -27,38 +28,32 @@ class SinglyLinkedList:
 
         self.tail = node
 
-def print_singly_linked_list(node, sep, fptr):
+
+def print_singly_linked_list(node, sep):
     while node:
-        fptr.write(str(node.data))
+        
+        print(str(node.data))
 
         node = node.next
 
         if node:
-            fptr.write(sep)
-
-#
-# Complete the 'reverse' function below.
-#
-# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
-# The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
-#
-
-#
-# For your reference:
-#
-# SinglyLinkedListNode:
-#     int data
-#     SinglyLinkedListNode next
-#
-#
+            print(sep)
 
 def reverse(llist):
-    print("Hello")
+    prev = None
 
-    # Write your code here
+    while llist is not None:
+    
+        nxt = llist.next
+        llist.next = prev
+        prev = llist
+        llist = nxt
+    
+    return prev
+
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     tests = int(input())
 
@@ -73,7 +68,7 @@ if __name__ == '__main__':
 
         llist1 = reverse(llist.head)
 
-        print_singly_linked_list(llist1, ' ', fptr)
-        fptr.write('\n')
+        print_singly_linked_list(llist1, ' ')
 
-    fptr.close()
+
+
